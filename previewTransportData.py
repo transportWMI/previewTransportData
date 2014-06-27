c80588cbf505b0458e6679e6879e10cd3c796ad0
pheonix <pheonix@pheonix-N82JV.(none)>
2014-06-27 18:30:49 +0200

    added support for processing and fitting data

--- previewTransportData.py
+++ previewTransportData.py
@@ -4,99 +4,159 @@ Created on Mon Jun 23 10:27:50 2014
 
 @author: hannes.maierflaig
 """
-from guidata.qt.QtGui import QVBoxLayout, QMainWindow, QWidget, QComboBox, QGridLayout, QHBoxLayout, QFileDialog, QPushButton, QTextEdit
+from guidata.qt.QtGui import QLabel, QIntValidator, QLineEdit, QCheckBox, QVBoxLayout, QMainWindow, QWidget, QComboBox, QGridLayout, QHBoxLayout, QFileDialog, QPushButton, QTextEdit
 from guidata.qt.QtCore import SIGNAL
 
-from guiqwt.curve import CurvePlot
 from guiqwt.plot import CurveDialog
 from guiqwt.builder import make
 
+from scipy import optimize
 import numpy as np
 import nptdms
 import re
 
-def plot( *items ):
-    win = CurveDialog(edit=False, toolbar=True)
-    plot = win.get_plot()
-    for item in items:
-        plot.add_item(item)
-    win.show()
-    win.exec_()
+import lib.transportdata as transdat
 
 class plotWidget(QWidget):
     """
-    Filter testing widget
-    parent: parent widget (QWidget)
-    x, y: NumPy arrays
-    func: function object (the signal filter to be tested)
+    Will be done soon
     """
-    def __init__(self, parent, x, y, func):
+    def __init__(self, parent):
         QWidget.__init__(self, parent)
         self.setMinimumSize(500, 500)
-        self.x = x
-        self.y = y
-        self.func = func
-        #---guiqwt related attributes:
-        self.plot = None
-        self.curve_item = None
-        #---
+
+        #flag initialization
+        self.average = False
+        self.symmetrize = False
+        self.antiSymmetrize = False
+        self.fitCosSq = False
+        self.fitCos = False
+        self.norm = False
+
+        #initialize data storage
+        self.symmStep = None
+        self.x = None
+        self.y = None
+        self.storageArray = []
+        
+        #initialize plot widget
         self.curveDialog = CurveDialog(edit=False,toolbar=True)
         self.curveDialog.get_itemlist_panel().show()
-        
-    def setup_widget(self, title):
-        #---Create the plot widget:
+              
         self.plot = self.curveDialog.get_plot()
-        self.curve_item = make.curve([], [], color='b')
-        self.plot.add_item(self.curve_item)
         self.plot.set_antialiasing(True)
-        #---
-        
-        button = QPushButton(u"Test filter: %s" % title)
-        self.connect(button, SIGNAL('clicked()'), self.process_data)
-        button2 = QPushButton(u"Symmetrize Data")
-        self.connect(button2, SIGNAL('clicked()'), self.symmetrizeData)
-        buttonWidget = QWidget()        
-        hlayout = QHBoxLayout()
-        hlayout.addWidget(self.curveDialog)
-        vlayout = QVBoxLayout()
-        buttonWidget.setLayout(vlayout)
-        vlayout.addWidget(button)
-        vlayout.addWidget(button2)
-        vlayout.addStretch(1)
-        hlayout.addWidget(buttonWidget)
-        self.setLayout(hlayout)
-        
-        self.update_curve()
-        
-    def symmetrizeData(self):
-        from lib.transportdata  import symmetrizeSignalUpDown, averageUpDownSweep
-        newY = symmetrizeSignalUpDown(self.y,int(len(self.y)/2))
-        print(len(self.y))
-        newX = averageUpDownSweep(self.x)
-#        np.linspace(min(self.x),max(self.x),len(self.y))
-        newcurve = make.curve(newX,newY, color='r')
-        self.plot.add_item(newcurve)
-        
-    def process_data(self):
-        self.y = self.func(self.y)
-        self.update_curve()
-        
-    def update_curve(self):
-        #---Update curve
-        self.curve_item.set_data(self.x, self.y)
-        self.curve_item.plot().replot()
         self.plot.do_autoscale()
-        #---
+        
+        # initialize layout    
+        self.checkBoxSymm = QCheckBox("Symmetrize")
+        self.checkBoxAnti = QCheckBox("Antisymmetrize")
+        self.checkBoxAverage = QCheckBox("Average")
+        self.checkBoxFitCosSq = QCheckBox("Fit Cos^2")
+        self.checkBoxFitCos = QCheckBox("Fit Cos")
+        self.checkBoxNorm = QCheckBox("Normalize")
+        
+        self.commitButton = QPushButton(u"Commit Changes")
+        
+        symmOffsetLabel = QLabel("Symmetry Step")
+        self.symmOffsetField = QLineEdit() 
+        self.symmOffsetField.setMaximumWidth(150)
+        self.symmOffsetField.setValidator(QIntValidator())
+        
+        
+        # connect SIGNALs
+        self.connect(self.commitButton, SIGNAL('clicked()'), self.commitChanges)
+        # should be removed if the button is used sooner or later
+        self.checkBoxAnti.stateChanged.connect(self.updateCheckboxes)        
+        self.checkBoxSymm.stateChanged.connect(self.updateCheckboxes)        
+        self.checkBoxNorm.stateChanged.connect(self.updateCheckboxes)        
+        self.checkBoxFitCos.stateChanged.connect(self.updateCheckboxes)        
+        self.checkBoxFitCosSq.stateChanged.connect(self.updateCheckboxes)        
+        self.checkBoxAverage.stateChanged.connect(self.updateCheckboxes)        
+                
+        # make layout        
+        #  first row
+        hlayout = QHBoxLayout()
+        hlayout.addStretch(1)
+        hlayout.addWidget(self.checkBoxNorm)
+        hlayout.addWidget(self.checkBoxAverage)
+        hlayout.addWidget(self.checkBoxSymm)
+        hlayout.addWidget(self.checkBoxAnti)
+        hlayout.addWidget(self.checkBoxFitCos)
+        hlayout.addWidget(self.checkBoxFitCosSq)
+        hlayout.addWidget(self.commitButton)
+        #  second row
+        hlayout2 = QHBoxLayout()
+        hlayout2.addStretch(5)
+        hlayout2.addWidget(symmOffsetLabel)
+        hlayout2.addWidget(self.symmOffsetField)
+        #  vertical layout
+        vlayout = QVBoxLayout()
+        vlayout.addLayout(hlayout)
+        vlayout.addLayout(hlayout2)
+        vlayout.addWidget(self.curveDialog)
+        self.setLayout(vlayout)
+        
+    # Here happens the stuff you want to apply to the data @ commit and befor plot
+    def processData(self):
+        x = self.x
+        y = self.y
+        if self.average:
+            x = x
+        if self.norm:
+            y = y-max(y)
+        if self.symmetrize:
+            y = transdat.SymmetrizeSignal(y)
+        if self.antiSymmetrize:
+            y = transdat.antiSymmetrizeSignal(y)
+        if self.fitCos:
+            fitfunc = lambda p,x: p[0]*np.sin(np.pi*(x-p[2])/p[1])+p[3]
+            errfunc = lambda p,x,y: fitfunc(p,x) - y
+            p0 = [max(y)-min(y),360,0,min(y)]
+            p1, success = optimize.leastsq(errfunc, p0[:],args=(x,y))
+            angle = np.linspace(x.min(),x.max(),4*len(x))
+            self.plot.add_item(make.curve(angle,fitfunc(p1,angle),color='r'))
+        if self.fitCosSq:
+            fitfunc = lambda p,x: p[0]*np.cos(np.pi*(x-p[1])/p[2])**2+p[3]
+            errfunc = lambda p,x,y: fitfunc(p,x) - y
+            p0 = [max(y)-min(y),0,180,(max(y)-min(y))/2]
+            p1, success = optimize.leastsq(errfunc, p0[:],args=(x,y),factor = 0.1)
+            print success
+            print p1
+            angle = np.linspace(x.min(),x.max(),4*len(x))
+            self.plot.add_item(make.curve(angle,fitfunc(p1,angle),color='r'))
+        return (x,y)
+    
+    def commitChanges(self):
+        self.updateCheckboxes(0);
+        (x,y) = self.processData()
+        self.plot.add_item(make.curve(x,y,color='b',marker='Ellipse', markerfacecolor='b'))
+        self.storageArray.append((x,y))
+        self.plot.do_autoscale()  
+        
+    def newData(self,x,y):
+        self.x = x
+        self.y = y
+        self.commitChanges()
+    
+    def updateCheckboxes(self,i):
+        self.average = self.checkBoxAverage.checkState()
+        self.symmetrize = self.checkBoxSymm.checkState()
+        self.antiSymmetrize = self.checkBoxAnti.checkState()
+        self.fitCosSq = self.checkBoxFitCosSq.checkState()
+        self.fitCos = self.checkBoxFitCos.checkState()
+        self.norm = self.checkBoxNorm.checkState()
 
-
+        
 class previewTransportDataWindow(QWidget):
+    '''
+    Will be done soon!
+    '''
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("previewTransportData")
 
         #Initialize Layout    
         layout = QGridLayout()
-        layout.setVerticalSpacing(0)
         self.setLayout(layout)
         self.fileTextWindow = QTextEdit()
         self.fileTextWindow.setMaximumWidth(750)
@@ -109,12 +169,12 @@ class previewTransportDataWindow(QWidget):
         self.yChannelBox.setMinimumWidth(250)
         button1 = QPushButton(u"Select File")
         button1.setMaximumWidth(100)
-        button2 = QPushButton(u"Plot")
-        button2.setMaximumWidth(100)
+        self.plotButton = QPushButton(u"Plot")
+        self.plotButton.setMaximumWidth(100)
         
         #connect SIGNALs
         self.connect(button1, SIGNAL('clicked()'), self.selectFile)
-        self.connect(button2, SIGNAL('clicked()'), self.plot)
+        self.connect(self.plotButton, SIGNAL('clicked()'), self.plot)
         
         #add to Layout
         layout.addWidget(self.fileTextWindow,0,0,1,3)
@@ -122,14 +182,19 @@ class previewTransportDataWindow(QWidget):
         layout.addWidget(self.groupBox,1,0)
         layout.addWidget(self.xChannelBox,1,1)
         layout.addWidget(self.yChannelBox,1,2)
-        layout.addWidget(button2,1,3)
+        layout.addWidget(self.plotButton,1,3)
         layout.columnStretch(4)
         
         #initialize store for TDMSfiles
         self.tdmsFile = None
         self.groupList = []
         self.ChannelList = []
-        self.widget = None
+        
+        #initialize plot widget
+        self.widget = plotWidget(self)
+        self.layout().addWidget(self.widget,2,0,1,4)
+        self.widget.commitButton.setEnabled(False)
+        self.plotButton.setEnabled(False)
         
     def selectFile(self):
         self.fileTextWindow.setText(QFileDialog.getOpenFileName(self,u"Open File","",u"TDMS (*.tdms)"))
@@ -151,13 +216,13 @@ class previewTransportDataWindow(QWidget):
         for channel in self.channelList:
             self.xChannelBox.addItem(re.search(r"'/'(.+)'",channel.path).group(1))
             self.yChannelBox.addItem(re.search(r"'/'(.+)'",channel.path).group(1))
+        self.plotButton.setEnabled(True)
         
     def plot(self):
+        self.widget.commitButton.setEnabled(True)
         x = self.channelList[self.xChannelBox.currentIndex()].data
         y = self.channelList[self.yChannelBox.currentIndex()].data
-        self.widget = plotWidget(self,x,y,lambda x: x/2)
-        self.widget.setup_widget("bla")        
-        self.layout().addWidget(self.widget,2,0,1,4)
+        self.widget.newData(x,y)
         
 def previewTransportData():
     """
@@ -172,16 +237,7 @@ def previewTransportData():
     win.show()
     _app.exec_()
     
-    from lib.transportdata  import antiSymmetrizeSignalUpDown
-    # onchange text_box_tmds_file: load Tdms File
-    
-    # onload tmds_file: Read Groups and populate group select box
-    
-    # onchange group_select_box: read channels of group and populate second select box
-    
-    # onchange channel_select_box;
-    #  read data and plot
-    
+     
     # onchange symmetrize(difference, sum, raw); onchange normalize(to 
     # max/min/mean/custom value); onchange average up_down_sweep:
     #   process raw data according to selected options and replace existing plot curve
@@ -191,20 +247,7 @@ def previewTransportData():
     
     # Long term: Fit sin/cos
     
-    curve = make.curve(x, y, "ab", "b")
-    range = make.range(-2, 2)
-    #disp0 = make.range_info_label(range, 'BR', u"x = %.1f ± %.1f cm",
-    #                              title="Range infos")
 
-    disp1 = make.computation(range, "BL", "trapz=%g",
-                             curve, lambda x,y: np.trapz(y,x))
-
-    disp2 = make.computations(range, "TL",
-                              [(curve, "min=%.5f", lambda x,y: y.min()),
-                               (curve, "max=%.5f", lambda x,y: y.max()),
-                               (curve, "avg=%.5f", lambda x,y: y.mean())])
-    legend = make.legend("TR")
-    plot( curve, range, disp1, disp2, legend)
 
 if __name__ == "__main__":
     previewTransportData()
